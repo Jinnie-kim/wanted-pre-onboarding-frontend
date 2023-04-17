2568ed8b8611818f014a118c0310b16d5f21ef2c
Jinnie <hjk7639@gmail.com>
2023-04-17 15:09:28 +0900

    [Feat] 로그인, 회원가입 페이지 이메일, 비밀번호 입력 데이터, 유효성 검사 처리

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,56 @@
+import { useEffect, useState } from 'react';
+import useValid from '../hooks/useValid';
 import { SignLayout, InputLayout, Button } from '../style/Sign.styled';
 
+interface FormProps {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
+  const [formValue, setFormValue] = useState<FormProps>({
+    email: '',
+    password: '',
+  });
+  const [isdeActive, setIsdeActive] = useState<boolean>(true);
+  const [validationText, isValid] = useValid(formValue);
+
+  useEffect(() => {
+    if (isValid.email && isValid.password) {
+      setIsdeActive(false);
+    } else {
+      setIsdeActive(true);
+    }
+  }, [isValid]);
+
+  const getUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
+
   return (
     <SignLayout>
       <h1>로그인</h1>
-      <InputLayout>
-        <label htmlFor="email">이메일</label>
-        <input type="text" id="email" data-testid="email-input" />
-      </InputLayout>
-      <InputLayout>
-        <label htmlFor="password">비밀번호</label>
-        <input type="password" id="password" data-testid="password-input" />
-      </InputLayout>
-      <Button type="button" data-testid="signin-button">
-        로그인하기
-      </Button>
+      <form>
+        <InputLayout>
+          <label htmlFor="email">
+            이메일 <span>{validationText.email}</span>
+          </label>
+          <input type="text" id="email" name="email" data-testid="email-input" onChange={getUserInfo} />
+        </InputLayout>
+        <InputLayout>
+          <label htmlFor="password">
+            비밀번호 <span>{validationText.password}</span>
+          </label>
+          <input type="password" id="password" name="password" data-testid="password-input" onChange={getUserInfo} />
+        </InputLayout>
+        <Button type="button" data-testid="signin-button" disabled={isdeActive} deActive={isdeActive}>
+          로그인하기
+        </Button>
+      </form>
     </SignLayout>
   );
 };
--- src/pages/Singup.tsx
+++ src/pages/Singup.tsx
@@ -1,18 +1,55 @@
+import { useEffect, useState } from 'react';
+import useValid from '../hooks/useValid';
 import { SignLayout, InputLayout, Button } from '../style/Sign.styled';
 
+interface FormProps {
+  email: string;
+  password: string;
+}
+
 const Signup = () => {
+  const [formValue, setFormValue] = useState<FormProps>({
+    email: '',
+    password: '',
+  });
+  const [isdeActive, setIsdeActive] = useState<boolean>(true);
+  const [validationText, isValid] = useValid(formValue);
+
+  useEffect(() => {
+    if (isValid.email && isValid.password) {
+      setIsdeActive(false);
+    } else {
+      setIsdeActive(true);
+    }
+  }, [isValid]);
+
+  const getUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
   return (
     <SignLayout>
       <h1>회원가입</h1>
-      <InputLayout>
-        <label htmlFor="email">이메일</label>
-        <input type="text" id="email" data-testid="email-input" />
-      </InputLayout>
-      <InputLayout>
-        <label htmlFor="password">비밀번호</label>
-        <input type="password" id="password" data-testid="password-input" />
-      </InputLayout>
-      <Button data-testid="signup-button">회원가입</Button>
+      <form>
+        <InputLayout>
+          <label htmlFor="email">
+            이메일 <span>{validationText.email}</span>
+          </label>
+          <input type="email" id="email" name="email" data-testid="email-input" onChange={getUserInfo} />
+        </InputLayout>
+        <InputLayout>
+          <label htmlFor="password">
+            비밀번호 <span>{validationText.password}</span>
+          </label>
+          <input type="password" id="password" name="password" data-testid="password-input" onChange={getUserInfo} />
+        </InputLayout>
+        <Button data-testid="signup-button" disabled={isdeActive} deActive={isdeActive}>
+          회원가입
+        </Button>
+      </form>
     </SignLayout>
   );
 };
