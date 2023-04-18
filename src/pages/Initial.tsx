309b7ab705a5e17ddab42c9f3ec21bbf042f63e8
Jinnie <hjk7639@gmail.com>
2023-04-19 02:09:02 +0900

    [Feat] 초기 진입 페이지에서 각 버튼 클릭하면 해당 페이지로 이동 구현

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,13 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import { InitialLayout, ButtonLayout, Button } from '../style/Initial.styled';
 
 const Initial = () => {
+  const navigate = useNavigate();
+
   return (
     <InitialLayout>
       <h1>Todo</h1>
 
       <ButtonLayout>
-        <Button>로그인</Button>
-        <Button>회원가입</Button>
+        <Button
+          onClick={() => {
+            navigate('/signin');
+          }}
+        >
+          로그인
+        </Button>
+        <Button
+          onClick={() => {
+            navigate('/signup');
+          }}
+        >
+          회원가입
+        </Button>
       </ButtonLayout>
     </InitialLayout>
   );
